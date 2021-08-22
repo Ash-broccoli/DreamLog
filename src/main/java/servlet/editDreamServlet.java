05974fca0827ebfee974bb0c857257806c61bbf9
Alyssa <heimlichera@bzz.ch>
2021-08-22 11:56:53 +0200

    edit dreams finally work

--- src/main/java/servlet/editDreamServlet.java
+++ src/main/java/servlet/editDreamServlet.java
@@ -64,7 +64,7 @@ import java.io.IOException;
 
                 new dreamDAO().update(d);
 
-                response.sendRedirect("dreams.jsp");
+                response.sendRedirect("index.jsp");
             }
 
         }
